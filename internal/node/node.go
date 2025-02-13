a36a59ab975553b90ad9f5913fbb127e31cf2034
Cristofer Luchtenberg <cristof3r.dk@gmail.com>
2025-02-13 19:05:34 -0300

    Add files via upload

--- internal/node/node.go
+++ internal/node/node.go
@@ -30,22 +30,34 @@ func NewNode(cfg *config.Config) *Node {
 	}
 }
 
+// inicia a eleicaoo quando o nó detecta a falta de um lider
 func (n *Node) StartElection(timeout time.Duration) {
 	for {
+		// tempo aleatorio para iniciar a eleicao
 		time.Sleep(time.Duration(rand.Intn(500)+500) * time.Millisecond)
+
 		n.Mu.Lock()
+		// Caso ja for lider ou tiver um ativo nao inicia a eleicao
 		if n.IsLeader || time.Since(n.LastActivity) < timeout {
 			n.Mu.Unlock()
 			continue
 		}
+
+		// inicia a eleicao
 		n.CurrentTerm++
-		n.VotedFor = n.ID
+		n.VotedFor = n.ID // Vota em si mesmo
 		votes := 1
 		n.Mu.Unlock()
 
+		// envia o pedido de voto para cada nó
 		for _, peer := range n.Peers {
 			go func(peer string) {
-				req, _ := json.Marshal(map[string]interface{}{"term": n.CurrentTerm, "candidate_id": n.ID})
+				req, _ := json.Marshal(map[string]interface{}{
+					"term":         n.CurrentTerm,
+					"candidate_id": n.ID,
+				})
+
+				// faz a request para solicitar os votos
 				resp, err := http.Post("http://"+peer+"/request-vote", "application/json", bytes.NewBuffer(req))
 				if err != nil {
 					return
@@ -54,9 +66,12 @@ func (n *Node) StartElection(timeout time.Duration) {
 
 				var result map[string]interface{}
 				json.NewDecoder(resp.Body).Decode(&result)
+
+				// se receber voto aumenta a contagem
 				if result["vote_granted"].(bool) {
 					n.Mu.Lock()
 					votes++
+					// se a quantidade de votos for maior que a quantidade de no divido por 2 vira lider
 					if votes > len(n.Peers)/2 && !n.IsLeader {
 						n.IsLeader = true
 						fmt.Printf("Nó %s é agora o líder no termo %d\n", n.ID, n.CurrentTerm)
@@ -68,16 +83,18 @@ func (n *Node) StartElection(timeout time.Duration) {
 	}
 }
 
+// envia os Heartbeats para cada nó
 func (n *Node) StartHeartbeat(interval time.Duration) {
 	for range time.Tick(interval) {
 		n.Mu.Lock()
+
 		if n.IsLeader {
 			for _, peer := range n.Peers {
 				go func(peer string) {
 					req, _ := json.Marshal(map[string]interface{}{"term": n.CurrentTerm})
 					resp, err := http.Post("http://"+peer+"/heartbeat", "application/json", bytes.NewBuffer(req))
 					if err == nil && resp.StatusCode == http.StatusOK {
-						fmt.Printf("Lider %s enviou heartbeat para %s\n", n.ID, peer)
+						fmt.Printf("Líder %s enviou heartbeat para %s\n", n.ID, peer)
 					}
 				}(peer)
 			}
@@ -86,9 +103,12 @@ func (n *Node) StartHeartbeat(interval time.Duration) {
 	}
 }
 
+// fica monitorando o lider
 func (n *Node) MonitorLeaderFailure(timeout time.Duration) {
 	for range time.Tick(timeout / 2) {
 		n.Mu.Lock()
+		// se nao for lider e nao recebeu heartbeat dentro do tempo
+		// inicia uma nova eleicao
 		if !n.IsLeader && time.Since(n.LastActivity) > timeout {
 			fmt.Printf("%s iniciou uma nova eleição...\n", n.ID)
 			go n.StartElection(timeout)
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -10,14 +10,17 @@ import (
 )
 
 func StartServer(n *node.Node, port int) {
+
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 
 	r.POST("/request-vote", func(c *gin.Context) {
+
 		var req struct {
 			Term        int
 			CandidateID string
 		}
+
 		if err := c.BindJSON(&req); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid request"})
 			return
@@ -26,16 +29,20 @@ func StartServer(n *node.Node, port int) {
 		n.Mu.Lock()
 		defer n.Mu.Unlock()
 
+		// se o termo do candidato for maior que o do nó atual da o voto
 		if req.Term > n.CurrentTerm {
 			n.CurrentTerm, n.VotedFor, n.IsLeader = req.Term, req.CandidateID, false
 			c.JSON(http.StatusOK, gin.H{"term": n.CurrentTerm, "vote_granted": true})
 		} else {
+			// nega o voto.
 			c.JSON(http.StatusOK, gin.H{"term": n.CurrentTerm, "vote_granted": false})
 		}
 	})
 
 	r.POST("/heartbeat", func(c *gin.Context) {
+
 		var req struct{ Term int }
+
 		if err := c.BindJSON(&req); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid request"})
 			return
@@ -44,6 +51,7 @@ func StartServer(n *node.Node, port int) {
 		n.Mu.Lock()
 		defer n.Mu.Unlock()
 
+		// verifica se o termo do lider for maior ou igual ao atual e atualiza o estado do nó
 		if req.Term >= n.CurrentTerm {
 			n.CurrentTerm, n.IsLeader, n.LastActivity = req.Term, false, time.Now()
 			c.JSON(http.StatusOK, gin.H{"term": n.CurrentTerm, "success": true})
